9edb4b0162fc03ffbd3fac69b0edddbf615379fb
kipler8654 <adfd130@gmail.com>
2023-07-29 20:52:02 +0300

    first commit

---
+++ src/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "hello";
+        String space = " ";
+        String name = "Carl";
+        System.out.println(s + space + name);
+        System.out.println("hello" + " " + "John");
+        System.out.println("My number is " + x);
+    }
+}
